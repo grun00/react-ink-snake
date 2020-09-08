80eb83b33a9be343a3e1dd028d61e203f183a7fa
grun00 <lucas.c.grunheidt@gmail.com>
2020-09-08 20:57:16 -0300

    made board

--- ui.js
+++ ui.js
@@ -1,12 +1,28 @@
 'use strict';
 const React = require('react');
+const PropTypes = require('prop-types');
 const {Text, Color, Box} = require('ink');
-const FIELD_SIZE = 16;
-const FIELD_ROW = [...new Array(FIELD_SIZE).keys()]
 
-const App = ({name = 'Stranger'}) => (
-  <Box>
-    <Text color='green'>Vim</Text><Text color='red'>Snake</Text>
+const FIELD_SIZE = 16;
+const FIELD_ROW = [...new Array(FIELD_SIZE).keys()];
+
+const App = () => (
+  <Box flexDirection='column' alignItems='center'>
+    <Text color='green'>Vi</Text><Text color='red'>Snake</Text>
+
+    <Box flexDirection='column' >
+      <Box flexDirection='column'>
+        {FIELD_ROW.map(y => (
+          <Box key={y}>
+            {FIELD_ROW.map(x => (
+              <Box key={x}>
+                <Text color={(x+y)%2==0? 'red' : 'green'}> . </Text>
+              </Box>
+            ))}
+          </Box>
+        ))}
+      </Box>
+    </Box>
   </Box>
 );
 
