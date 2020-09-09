5346b74c46445d6a8c03e8b49d956a2aff645691
grun00 <lucas.c.grunheidt@gmail.com>
2020-09-08 21:44:38 -0300

    Added body parts and goal

--- ui.js
+++ ui.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const React = require('react');
 const PropTypes = require('prop-types');
 const {Text, Color, Box} = require('ink');
@@ -6,24 +7,59 @@ const {Text, Color, Box} = require('ink');
 const FIELD_SIZE = 16;
 const FIELD_ROW = [...new Array(FIELD_SIZE).keys()];
 
-const App = () => (
-  <Box flexDirection='column' alignItems='center'>
-    <Text color='green'>Vi</Text><Text color='red'>Snake</Text>
+const food = {
+  x: Math.round(Math.random()*FIELD_SIZE),
+  y: Math.round(Math.random()*FIELD_SIZE)
+}
 
-    <Box flexDirection='column' >
-      <Box flexDirection='column'>
-        {FIELD_ROW.map(y => (
-          <Box key={y}>
-            {FIELD_ROW.map(x => (
-              <Box key={x}>
-                <Text color={(x+y)%2==0? 'red' : 'green'}> . </Text>
+function getItem(x, y, snakeSegments){
+  if(x === food.x && y === food.y){
+    return <Text color='red'> O </Text>
+  }
+
+  for(const segment of snakeSegments){
+    if(x === segment.x && y === segment.y){
+      return <Text color='green'> x </Text>
+    }
+  };
+
+  return <Text color='#761298'> . </Text>
+}
+
+const App = () => {
+  const [snakeSegments, setSnakeSegments] = React.useState([
+      {
+        x: 8,
+        y: 8
+      },
+      {
+        x: 8,
+        y: 7
+      },
+      {
+        x: 8,
+        y: 6
+      }
+  ])
+
+  return (
+    <Box flexDirection='column' alignItems='center'>
+      <Text color='green'>Vi</Text><Text color='red'>Snake</Text>
+        <Box flexDirection='column' >
+          <Box flexDirection='column'>
+            {FIELD_ROW.map(y => (
+              <Box key={y}>
+                {FIELD_ROW.map(x => (
+                  <Box key={x}>
+                    {getItem(x, y, snakeSegments)}
+                  </Box>
+                ))}
               </Box>
-            ))}
+              ))}
           </Box>
-        ))}
-      </Box>
+        </Box>
     </Box>
-  </Box>
-);
+  )
+};
 
 module.exports = App;
